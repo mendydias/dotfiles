402eca3e2fd7cddad98efca984f4a805428bdc12
Umendya "mendydias" Dias <dev.mendy.das@gmail.com>
2024-05-29 13:50:10 +0530

    changed the colorscheme...again..

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -7,13 +7,13 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'Afterglow (Gogh)'
+config.color_scheme = 'Ayu Dark (Gogh)'
 config.colors = {
 	cursor_bg='white',
 	cursor_fg='black',
 	compose_cursor='orange'
 }
-config.window_background_opacity=0.75
+config.window_background_opacity=0.85
 config.window_decorations = "RESIZE"
 config.font = wezterm.font('JetBrainsMono Nerd Font', {weight='Bold'})
 config.font_size = 15

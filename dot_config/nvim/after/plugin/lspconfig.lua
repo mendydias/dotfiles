985e7a06ddc3b3a6c59fec33a9bcada60ee61032
Umendya "mendydias" Dias <dev.mendy.das@gmail.com>
2024-06-03 01:40:21 +0530

    changed lspconfig keybindings

--- dot_config/nvim/after/plugin/lspconfig.lua
+++ dot_config/nvim/after/plugin/lspconfig.lua
@@ -21,7 +21,7 @@ local on_attach = function(_, bufnr)
   nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
   nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
   nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-  nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+  nmap('<leader>td', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
   nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
   nmap('<leader>sw', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 

7e638483e68a8b05b8f2682aec705dd74ed20fb9
Umendya "mendydias" Dias <dev.mendy.das@gmail.com>
2024-06-03 01:58:23 +0530

    Testing a new binding for codeium

--- dot_config/nvim/lua/mendy/plugins/codeium.lua
+++ dot_config/nvim/lua/mendy/plugins/codeium.lua
@@ -2,7 +2,7 @@ return {
     "Exafunction/codeium.vim",
     config = function()
         vim.g.codeium_no_map_tab = 1
-        vim.keymap.set('i', 'kk', function() return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
+        vim.keymap.set('i', 'kk', ":call codeium#Accept()<CR><Esc>", { expr = true, silent = true })
         vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end,
             { expr = true, silent = true })
         vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end,

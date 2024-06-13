e35c75056fa042e52cb9ce7d7a538dbc61341bf9
Umendya "mendydias" Dias <dev.mendy.das@gmail.com>
2024-06-13 11:42:11 +0530

    setup proper bg support on nordic

--- dot_config/nvim/lua/mendy/themes/nordic.lua
+++ dot_config/nvim/lua/mendy/themes/nordic.lua
@@ -4,5 +4,10 @@ return {
         priority = 1000,
         config = function()
         require 'nordic' .load()
+    end,
+    config = function ()
+        require 'nordic' .setup() {
+            transparent_bg = true
+        }
     end
 }

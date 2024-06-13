fde4137059fcd6ed72183eab707a141444719222
Umendya "mendydias" Dias <dev.mendy.das@gmail.com>
2024-06-13 11:43:58 +0530

    typo in nordic lua setup

--- dot_config/nvim/lua/mendy/themes/nordic.lua
+++ dot_config/nvim/lua/mendy/themes/nordic.lua
@@ -1,13 +1,11 @@
 return {
     'AlexvZyl/nordic.nvim',
     lazy = false,
-        priority = 1000,
-        config = function()
-        require 'nordic' .load()
-    end,
-    config = function ()
-        require 'nordic' .setup() {
+    priority = 1000,
+    config = function()
+        require 'nordic'.setup() {
             transparent_bg = true
         }
+        require 'nordic'.load()
     end
 }

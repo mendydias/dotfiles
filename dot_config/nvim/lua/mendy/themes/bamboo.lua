b44edfc1ad346a00d7d44a62c0a06e254770eeb4
mendydias <dev.mendy.das@gmail.com>
2024-06-17 02:53:11 +0530

    added 12 colorschemes

---
+++ dot_config/nvim/lua/mendy/themes/bamboo.lua
@@ -0,0 +1,12 @@
+return {
+    'ribru17/bamboo.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+        require('bamboo').setup {
+            -- optional configuration here
+            transparent = true
+        }
+        require('bamboo').load()
+    end,
+}
---
+++ dot_config/nvim/lua/mendy/themes/zephyr.lua
@@ -0,0 +1,4 @@
+return {
+    'glepnir/zephyr-nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', opt = true },
+}

b85889d36b7bd1db482607e0569cc3efe5f89510
Umendya Dias <dev.mendy.das@gmail.com>
2024-07-15 16:48:41 +0530

    Added Hyprland configs

--- dot_config/nvim/after/plugin/lspconfig.lua
+++ dot_config/nvim/after/plugin/lspconfig.lua
@@ -1,3 +1,5 @@
+require("java").setup {}
+
 vim.keymap.set('n', '<leader>ld', vim.diagnostic.open_float, { desc = 'Open diagnostic float' })
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next)

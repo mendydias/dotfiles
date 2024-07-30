3437a2b31437956343c20237cc4ec1f8591b2b90
Umendya Dias <dev.mendy.das@gmail.com>
2024-07-30 11:52:55 +0530

    modified terminal emulator to wezterm

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
@@ -7,23 +7,16 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'Bamboo'
-config.colors = {
-	cursor_bg='white',
-	cursor_fg='black',
-	compose_cursor='orange'
-}
-config.window_background_opacity=0.9
-config.window_decorations = "RESIZE"
-config.font = wezterm.font('FiraCode Nerd Font', {weight='Medium'})
-config.font_size = 16
+config.color_scheme = "Tokyo Night Storm"
+config.window_background_opacity = 0.67
+config.window_decorations = "NONE"
+config.font = wezterm.font("FiraCode Nerd Font", { weight = "Medium" })
+config.font_size = 9
 config.hide_tab_bar_if_only_one_tab = true
 config.window_padding = {
-  left = 8,
-  right = 8,
-  top = 8,
-  bottom = 8,
+	bottom = 24,
 }
+config.enable_scroll_bar = false
 -- config.text_background_opacity = 0.67
 
 -- and finally, return the configuration to wezterm

ad97c146c200e5215002d4398549d179c09eaabc
Umendya Dias <dev.mendy.das@gmail.com>
2024-08-01 13:31:23 +0530

    Update .wezterm.lua

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -7,7 +7,7 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = "OneHalfLight"
+config.color_scheme = "Oxocarbon Dark (Gogh)"
 config.window_background_opacity = 0.67
 config.window_decorations = "NONE"
 config.font = wezterm.font("FiraCode Nerd Font", { weight = "Medium" })

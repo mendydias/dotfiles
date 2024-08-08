46791e9c7bc2c5c7934ff26a1528a23c87dccead
Umendya Dias <dev.mendy.das@gmail.com>
2024-08-09 00:32:05 +0530

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,7 +1,3 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-<<<<<<< Updated upstream
-vim.cmd("colorscheme catppuccin")
-=======
 vim.cmd("colorscheme bamboo")
->>>>>>> Stashed changes

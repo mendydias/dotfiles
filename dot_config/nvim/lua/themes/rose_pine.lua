f806b7892bc70862d24af08e9220f3f00a1adcf2
Umendya Dias <dev.mendy.das@gmail.com>
2024-08-10 10:00:15 +0530

    Update .config/nvim/lazy-lock.json Update .config/nvim/lazyvim.json Add .config/nvim/lua/plugins/multi.lua Update .config/nvim/lua/themes/rose_pine.lua

--- dot_config/nvim/lua/themes/rose_pine.lua
+++ dot_config/nvim/lua/themes/rose_pine.lua
@@ -7,7 +7,7 @@ return {
     --- @usage 'main'|'moon'|'dawn'
     dark_variant = "main",
     bold_vert_split = false,
-    dim_nc_background = true,
+    dim_nc_background = false,
     disable_background = false,
     disable_float_background = false,
     disable_italics = true,

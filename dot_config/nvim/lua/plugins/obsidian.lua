6032540c3d04e18a8b6d8c5e9bfa0bde75b78f01
Umendya Dias <dev.mendy.das@gmail.com>
2024-09-08 12:35:32 +0530

    Added everything to obsidian and added a pomodoro timer

--- dot_config/nvim/lua/plugins/obsidian.lua
+++ dot_config/nvim/lua/plugins/obsidian.lua
@@ -15,13 +15,61 @@ return {
     -- Required.
     "nvim-lua/plenary.nvim",
   },
+  keys = {
+    { "<leader>oo", "<cmd>ObsidianOpen<cr>", desc = "Open Obsidian" },
+    { "<leader>on", "<cmd>ObsidianNew<cr>", desc = "Create a new Obsidian Document" },
+    { "<leader>os", "<cmd>ObsidianSearch<cr>", desc = "Search Obsidian" },
+    { "<leader>ot", "<cmd>ObsidianTag<cr>", desc = "Search Obsidian Tags" },
+    { "<leader>of", "<cmd>ObsidianQuickSwitch<cr>", desc = "Obsidian Find Files" },
+    { "<leader>op", "<cmd>ObsidianPasteImg<cr>", desc = "Obsidian Paste Image" },
+    { "<leader>or", "<cmd>ObsidianRename<cr>", desc = "Obsidian Rename current note" },
+    {
+      "<leader>obl",
+      "<cmd>ObsidianBacklinks<cr>",
+      desc = "Obsidian list backlinks to the current note",
+    },
+    { "<M-t>", "<cmd>ObsidianTemplate _layout_2<cr>/o1df<cr>cc", desc = "Insert Obsidian Template into file" },
+  },
   opts = {
     workspaces = {
       {
-        name = "my notes",
-        path = "$HOME/Documents/3.resources/1.notes.bak/",
+        name = "second-brain-git",
+        path = "$HOME/Documents/3.resources/1.notes.new/",
       },
     },
+    -- 'workspaces'. For example:
+    -- dir = "~/vaults/work",
+
+    -- Optional, if you keep notes in a specific subdirectory of your vault.
+    notes_subdir = "3.notes/",
+    -- Either 'wiki' or 'markdown'.
+    preferred_link_style = "markdown",
+    -- Optional, boolean or a function that takes a filename and returns a boolean.
+    -- `true` indicates that you don't want obsidian.nvim to manage frontmatter.
+    -- disable_frontmatter = false,
+    -- Optional, alternatively you can customize the frontmatter data.
+    --@return table
+    note_frontmatter_func = function(note)
+      -- Add the title of the note as an alias.
+      if note.title then
+        note:add_alias(note.title)
+      end
+
+      local out = { id = note.id, aliases = note.aliases, tags = note.tags }
+
+      -- `note.metadata` contains any manually added fields in the frontmatter.
+      -- So here we just make sure those fields are kept in the frontmatter.
+      if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+        for k, v in pairs(note.metadata) do
+          out[k] = v
+        end
+      end
+
+      return out
+    end,
+    -- Optional, set the log level for obsidian.nvim. This is an integer corresponding to one of the log
+    -- levels defined by "vim.log.levels.*".
+    log_level = vim.log.levels.INFO,
     completion = {
       -- Set to false to disable completion.
       nvim_cmp = true,
@@ -32,7 +80,7 @@ return {
       -- Where to put new notes created from completion. Valid options are
       --  * "current_dir" - put new notes in same directory as the current buffer.
       --  * "notes_subdir" - put new notes in the default notes subdirectory.
-      -- new_notes_location = "current_dir",
+      new_notes_location = "notes_subdir",
 
       -- Control how wiki links are completed with these (mutually exclusive) options:
       -- 1. Whether to add the note ID during completion.
@@ -46,7 +94,7 @@ return {
       -- 3. Whether to only use paths during completion.
       -- E.g. "[[Foo" completes to "[[notes/foo]]" assuming "notes/foo.md" is the path of the note.
       -- Mutually exclusive with 'prepend_note_id' and 'prepend_note_path'.
-      -- use_path_only = false, -- Disable the ugly note id
+      use_path_only = false, -- Disable the ugly note id
     },
     mappings = {
       -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
@@ -75,13 +123,55 @@ return {
         end,
       },
     },
+    -- Optional, by default when you use `:ObsidianFollowLink` on a link to an external
+    -- URL it will be ignored but you can customize this behavior here.
+    ---@param url string
+    follow_url_func = function(url)
+      -- Open the URL in the default web browser.
+      -- vim.fn.jobstart({ "open", url }) -- Mac OS
+      vim.fn.jobstart({ "xdg-open", url }) -- linux
+      -- vim.cmd(':silent exec "!start ' .. url .. '"') -- Windows
+      -- vim.ui.open(url) -- need Neovim 0.10.0+
+    end,
+    -- Optional, by default when you use `:ObsidianFollowLink` on a link to an image
+    -- file it will be ignored but you can customize this behavior here.
+    ---@param img string
+    follow_img_func = function(img)
+      -- vim.fn.jobstart({ "qlmanage", "-p", img }) -- Mac OS quick look preview
+      vim.fn.jobstart({ "xdg-open", img }) -- linux
+      -- vim.cmd(':silent exec "!start ' .. url .. '"') -- Windows
+    end,
     finder = "telescope.nvim",
-
     -- Optional, sort search results by "path", "modified", "accessed", or "created".
     -- The recommend value is "modified" and `true` for `sort_reversed`, which means, for example,
     -- that `:ObsidianQuickSwitch` will show the notes sorted by latest modified time
     sort_by = "modified",
     sort_reversed = true,
+    picker = {
+      -- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', or 'mini.pick'.
+      name = "telescope.nvim",
+      -- Optional, configure key mappings for the picker. These are the defaults.
+      -- Not all pickers support all mappings.
+      note_mappings = {
+        -- Create a new note from your query.
+        new = "<C-x>",
+        -- Insert a link to the selected note.
+        insert_link = "<C-l>",
+      },
+      tag_mappings = {
+        -- Add tag(s) to current note.
+        tag_note = "<C-x>",
+        -- Insert a tag at the current location.
+        insert_tag = "<C-l>",
+      },
+    },
+    -- Set the maximum number of lines to read from notes on disk when performing certain searches.
+    search_max_lines = 1000,
+    -- Optional, determines how certain commands open notes. The valid options are:
+    -- 1. "current" (the default) - to always open in the current window
+    -- 2. "vsplit" - to open in a vertical split if there's not already a vertical split
+    -- 3. "hsplit" - to open in a horizontal split if there's not already a horizontal split
+    open_notes_in = "vsplit",
     -- Specify how to handle attachments.
     attachments = {
       -- The default folder to place images in via `:ObsidianPasteImg`.
@@ -97,7 +187,7 @@ return {
       -- Optional, if you want to change the date format of the default alias of daily notes.
       alias_format = "%B %-d, %Y",
       -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
-      template = nil,
+      template = "main_template.md",
     },
     ui = {
       enable = true, -- set to false to disable all additional syntax features

b1d04fc1445d237df09772412b32eace119b617d
Umendya Dias <dev.mendy.das@gmail.com>
2024-09-08 11:49:47 +0530

    Forgot to include some local changes to the neovim and hypr keybindings" "

--- dot_config/nvim/lua/themes/catppuccin.lua
+++ dot_config/nvim/lua/themes/catppuccin.lua
@@ -4,7 +4,7 @@ return {
   priority = 1000,
   config = function()
     require("catppuccin").setup({
-      flavour = "latte", -- latte, frappe, macchiato, mocha
+      flavour = "mocha", -- latte, frappe, macchiato, mocha
       background = { -- :h background
         light = "latte",
         dark = "mocha",

cde37221be650b46c2f5036ba1a5b175e2d97c0e
Umendya Dias <dev.mendy.das@gmail.com>
2024-10-28 18:18:46 +0530

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/oil.lua

--- dot_config/nvim/lua/plugins/oil.lua
+++ dot_config/nvim/lua/plugins/oil.lua
@@ -1,7 +1,36 @@
+local detail = false
+
+function _G.get_oil_winbar()
+  local dir = require("oil").get_current_dir()
+  if dir then
+    return vim.fn.fnamemodify(dir, ":~")
+  else
+    -- If there is no current directory (e.g. over ssh), just show the buffer name
+    return vim.api.nvim_buf_get_name(0)
+  end
+end
+
 return {
   "stevearc/oil.nvim",
   config = function()
-    require("oil").setup()
+    require("oil").setup({
+      keymaps = {
+        ["gd"] = {
+          desc = "Toggle file detail view",
+          callback = function()
+            detail = not detail
+            if detail then
+              require("oil").set_columns({ "icon", "permissions", "size", "mtime" })
+            else
+              require("oil").set_columns({ "icon" })
+            end
+          end,
+        },
+      },
+      win_options = {
+        winbar = "%!v:lua.get_oil_winbar()",
+      },
+    })
   end,
   keys = {
     { "-", ":Oil<CR>", desc = "Opens the Oil file browser" },

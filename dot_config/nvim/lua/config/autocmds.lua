97b33bd4095e53614675c9a2f14d5a5a9ab87ffb
Umendya Dias <dev.mendy.das@gmail.com>
2024-11-09 16:46:16 +0530

    Update .config/nvim/lua/config/autocmds.lua Update .config/nvim/snippets/markdown.json Update .config/nvim/spell/en.utf-8.add Update .config/nvim/spell/en.utf-8.add.spl

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,10 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local screenCenter = vim.api.nvim_create_augroup("ScreenCenter", {})
+vim.api.nvim_create_autocmd("InsertEnter", {
+  pattern = "markdown",
+  group = screenCenter,
+  command = "norm zz",
+})

22ee2cd38323b98b39e357ed44737de0e5e71897
Umendya Ranmal Dias <dev.mendy.das@gmail.com>
2024-12-15 13:13:28 +0530

    Update .config/nvim/lazy-lock.json Update .config/nvim/lazyvim.json Update .config/nvim/lua/config/keymaps.lua

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -2,9 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- I like centering my scrolls
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<M-a>", "I#<Esc>$")
 vim.keymap.set("n", "<M-x>", "^x$")
 

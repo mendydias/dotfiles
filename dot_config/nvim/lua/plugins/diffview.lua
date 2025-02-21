4c2c422a48510a60fdb40820eeb4795978c53bb7
Umendya Dias <dev.mendy.das@gmail.com>
2025-02-21 16:53:37 +0530

    Update .config/nvim/lazy-lock.json Update .config/nvim/lazyvim.json Update .config/nvim/lua/plugins/diffview.lua

--- dot_config/nvim/lua/plugins/diffview.lua
+++ dot_config/nvim/lua/plugins/diffview.lua
@@ -4,4 +4,5 @@ return {
     { "<leader>gdo", "<cmd>DiffviewOpen<cr>", { desc = "Open diff view tabs" } },
     { "<leader>gdc", "<cmd>DiffviewClose<cr>", { desc = "Close diff view tabs" } },
   },
+  lazy = false,
 }

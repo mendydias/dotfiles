54141f1ceaeb8ea1e28ec2e408c076b21981888a
Umendya Ranmal Dias <dev.mendy.das@gmail.com>
2025-05-14 06:59:39 +0530

    Update .config/nvim/init.lua Update .config/nvim/lazy-lock.json Add .config/nvim/lua/plugins/yazi.lua Add .config/nvim/lua/themes/jellybeans.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,4 +1,4 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
-vim.cmd("colorscheme catppuccin")
+vim.cmd("colorscheme kanso-zen")
---
+++ dot_config/nvim/lua/themes/jellybeans.lua
@@ -0,0 +1,25 @@
+return {
+  "wtfox/jellybeans.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {}, -- Optional
+}
+
+-- Options for opts
+-- {
+--   transparent = false,
+--   italics = true,
+--   flat_ui = true, -- toggles "flat UI" for pickers
+--   background = {
+--     dark = "jellybeans", -- default dark palette
+--     light = "jellybeans_light", -- default light palette
+--   },
+--   plugins = {
+--     all = false,
+--     auto = true, -- will read lazy.nvim and apply the colors for plugins that are installed
+--   },
+--   on_highlights = function(highlights, colors)
+--   end,
+--   on_colors = function(colors)
+--   end,
+-- }
